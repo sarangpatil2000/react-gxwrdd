66a5c4c347a125f2b987862be4a55f73eb5a1239
sarangpatil2000 <sarangpatil2000@gmail.com>
2019-11-20 12:57:44 +0800

    1 file modified

--- Sarang.jsx
+++ Sarang.jsx
@@ -7,19 +7,26 @@ class Sarang extends Component{
   constructor(){
     super();
     this.state = {
-      name : "Sarang patil"
+      name : "Sarang patil",
+      count : 0
     };
   }
   handleChange(event){
     this.setState({name : event.target.value});
-};
+  }
+  handleButtonClicked(){
+    this.setState({count : this.state.count + 1});
+  }
+
 render(){
   return(
     <div>
     <input  OnChange={(event) => this.handleChange(event)}/>
     <h1>Hello {this.state.name}!</h1>
+    <span class="badge badge-primary m-2">{this.state.count}</span>
+    <button onClick={(event) => this.handleButtonClicked()} class="btn btn-primary">Increment</button>
     </div>
   );
-}
-}
+};
+};
 export default Sarang;
