dffa114e342f0ac3643538f95326deb56d53f3d3
sarangpatil2000 <sarangpatil2000@gmail.com>
2019-11-22 22:05:14 +0800

    2 files modified

--- Counter.jsx
+++ Counter.jsx
@@ -3,21 +3,20 @@ import {render} from 'react-dom';
 import "bootstrap/dist/css/bootstrap.css";
 
 class Counter extends Component{
-  constructor(){
-    super();
-    this.state ={
-      count : 0
+    state ={
+      value : this.props.value
     }
-  };
+  
 
   handleClickEvent = event =>{
-    this.setState({count:this.state.count + 1});
+    this.setState({value:this.state.value + 1});
   }
 
   render(){
+    console.log("Props", this.props);
     return(
       <div>
-      <span className="badge badge-primary m-2">{this.state.count}</span>
+      <span className="badge badge-primary m-2">{this.state.value}</span>
       <button className="btn btn-primary m-1" onClick={()=>this.handleClickEvent(event)}>Increment</button>
       </div>
     )
--- Counters.jsx
+++ Counters.jsx
@@ -6,19 +6,20 @@ class Counters extends Component{
 constructor(){
   super()
   this.state={
-    counterList : {
-
-    }
+    counterList : [
+      {id:0, valueSet:4},
+      {id:1, valueSet:0},
+      {id:2, valueSet:40},
+      {id:3, valueSet:0},
+      ]
   }
 };
 
 render(){
   return(
     <div>
-    <Counter/>
-    <Counter/>
-    <Counter/>
-    <Counter/>
+    {this.state.counterList.map(counter => 
+    <Counter key={counter.id} value={counter.valueSet} selected={true}/>)}
     </div>
   )
 }
