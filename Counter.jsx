4f955b80d95c7a78001e18a3b73fb21a08243d51
sarangpatil2000 <sarangpatil2000@gmail.com>
2019-11-26 22:40:17 +0800

    2 files modified

--- Counter.jsx
+++ Counter.jsx
@@ -3,23 +3,12 @@ import {render} from 'react-dom';
 import "bootstrap/dist/css/bootstrap.css";
 
 class Counter extends Component{
-    state ={
-      value : this.props.counterItem.valueSet,
-      id:this.props.counterItem.id
-    }
-  
-
-  handleClickEvent = event =>{
-    this.setState({value:this.state.value + 1});
-  }
-
-
   render(){
     return(
       <div>
-      <span className="badge badge-primary m-2">{this.state.value}</span>
-      <button className="btn btn-primary m-1" onClick={()=>this.handleClickEvent(event)}>Increment</button>
-      <button className="btn btn-danger m-1" onClick={()=>this.props.onDelete(this.state.id)}>Delete</button>
+      <span className="badge badge-primary m-2">{this.props.counterItem.valueSet}</span>
+      <button className="btn btn-primary m-1" onClick={()=>this.props.onIncrement}>Increment</button>
+      <button className="btn btn-danger m-1" onClick={()=>this.props.onDelete}>Delete</button>
       </div>
     )
   };
--- Counters.jsx
+++ Counters.jsx
@@ -8,12 +8,19 @@ constructor(){
   this.state={
     counterList : [
       {id:0, valueSet:0},
-      {id:1, valueSet:0},
-      {id:2, valueSet:0},
+      {id:1, valueSet:1},
+      {id:2, valueSet:50},
       {id:3, valueSet:0},
       ]
   }
 };
+
+
+onIncrement=(id)=>{
+  const newValue = this.state.counterList.filter(c=>c.id == id).valueSet+1;
+  this.state.counterList[id].valueSet = newValue;
+  this.setState({this.state.counterList});
+}
 
 handleDelete =(clickedButtonNo)=>{
     const counterList = this.state.counterList.filter(c=>c.id!=clickedButtonNo);
@@ -24,7 +31,8 @@ render(){
   return(
     <div>
     {this.state.counterList.map(counter => 
-    <Counter key={counter.id} onDelete={this.handleDelete} counterItem={counter}/>)}
+    <Counter key={counter.id} onDelete={this.handleDelete} counterItem={counter} value={counter.valueSet}
+    onIncrement={this.handleIncrement{counter.id}/>)}
     </div>
   )
 }
