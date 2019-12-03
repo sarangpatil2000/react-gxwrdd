79a3fae337edc389a2ed153664fcef1127aff81b
sarangpatil2000 <sarangpatil2000@gmail.com>
2019-12-04 06:23:29 +0800

    1 file modified

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import { render } from 'react-dom';
 import Sarang from './Sarang';
 import Counters from './Counters';
+import NavBar from './NavBar';
 import "bootstrap/dist/css/bootstrap.css";
 
 class App extends Component {
@@ -15,6 +16,7 @@ class App extends Component {
   render() {
     return (
       <React.Fragment>
+        <NavBar/>
         <Counters/>
       </React.Fragment>
     );
